3893487ba6162db736b128201f688fbf0c9de6e1
Diego Vieira <diegovieira.dvf@gmail.com>
2023-05-16 20:33:46 -0300

    Set up a color trail effect in each grid cell

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ function createGrid() {
         for (let j = 0; j < 16; j++) {
             const div = document.createElement("div");
             div.classList.add("grid-square");
+            div.addEventListener("mouseover", function (e) {
+                e.target.style.backgroundColor = "black";
+            })
             container.appendChild(div);
         }
     }
